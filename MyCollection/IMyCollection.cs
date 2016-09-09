e31b58fd1396bf1409488dcaedcf43cb8874db72
Johan Svensson <johan@atomicplayboy.net>
2016-09-09 10:57:59 +0200

    Add project files.

---
+++ MyCollection/IMyCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection
+{
+    interface IMyCollection<T>
+    {
+        void Add(T item);  // Enqueue/Push
+        T Get();  // Peek
+        T GetAndRemove(); // Dequeue/Pop
+        int Count { get; }
+        void Clear();
+        bool Contains(T item);
+    }
+}
